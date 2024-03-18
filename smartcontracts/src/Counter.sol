7d81a6c62e06a68e5de01f2b75f4bb98847dd96d
Albino Aveleda <albino@bino.eng.br>
2024-03-18 20:26:29 -0300

    fix deploy.py and counter.sol

--- smartcontracts/src/Counter.sol
+++ smartcontracts/src/Counter.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.8.18;
 
 contract Counter {
     uint256 public number;
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
